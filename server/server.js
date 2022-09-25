d55da4b442203b348e82b92c0c33ef9f3bffee18
Matt <mwhlavaty@gmail.com>
2022-09-25 15:09:05 -0700

    set up concurrent server starts, etc.

--- server/server.js
+++ server/server.js
@@ -1,15 +1,16 @@
+const path = require("path");
 const express = require("express");
 const { ApolloServer } = require("apollo-server-express");
 const { typeDefs, resolvers } = require("./schemas");
 const db = require("./config/connection");
-const {authMiddleware} = require('./utils/auth')
+const { authMiddleware } = require("./utils/auth");
 
 const PORT = process.env.PORT || 3001;
 // create a new Apollo server and pass in our schema data
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: ({req}) => req.headers
+  context: ({ req }) => req.headers,
 });
 
 const app = express();
@@ -23,6 +24,15 @@ const startApolloServer = async (typeDefs, resolvers) => {
   // integrate our Apollo server with the Express application as middleware
   server.applyMiddleware({ app });
 };
+
+// Serve up static assets
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+}
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
 db.once("open", () => {
   app.listen(PORT, () => {
