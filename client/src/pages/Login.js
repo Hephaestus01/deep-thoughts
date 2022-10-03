b41ec8fa4a9e62ea554995a0f7d4e048db30208e
Matt <mwhlavaty@gmail.com>
2022-10-02 19:52:26 -0700

    transfer from snapshop

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -2,9 +2,10 @@ import React, { useState } from 'react';
 import { useMutation } from '@apollo/client';
 import { LOGIN_USER } from '../utils/mutations';
 
+import Auth from '../utils/auth';
+
 const Login = (props) => {
   const [formState, setFormState] = useState({ email: '', password: '' });
-
   const [login, { error }] = useMutation(LOGIN_USER);
 
   // update state based on form input changes
@@ -18,19 +19,25 @@ const Login = (props) => {
   };
 
   // submit form
-const handleFormSubmit = async (event) => {
-  event.preventDefault();
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
 
-  try {
-    const { data } = await login({
-      variables: { ...formState },
+    try {
+      const { data } = await login({
+        variables: { ...formState },
+      });
+
+      Auth.login(data.login.token);
+    } catch (e) {
+      console.error(e);
+    }
+
+    // clear form values
+    setFormState({
+      email: '',
+      password: '',
     });
-
-    console.log(data);
-  } catch (e) {
-    console.error(e);
-  }
-};
+  };
 
   return (
     <main className="flex-row justify-center mb-4">
@@ -61,6 +68,7 @@ const handleFormSubmit = async (event) => {
                 Submit
               </button>
             </form>
+
             {error && <div>Login failed</div>}
           </div>
         </div>
